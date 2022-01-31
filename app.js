1e951f83f0129c8c83b6d37c30587a3b0cf418ea
Daniel Hernández <danielhernandez9980@gmail.com>
2022-01-30 21:45:14 -0600

    Boton Del Last funcionando y mostrar alertas

--- app.js
+++ app.js
@@ -1,101 +1,94 @@
-// definicion de las constantes 
+// definicion de las constantes
 
-// ocultar o mostrar div que contiene la lista 
-const  hide = document.querySelector('#hideElements');
-const listDiv = document.querySelector('#list');
+// ocultar o mostrar div que contiene la lista
+const hide = document.querySelector("#hideElements");
+const listDiv = document.querySelector("#list");
 
-// ahora las constantes para agregar elementos a la lista 
-const addItemInput = document.querySelector('#addItem'); //input
-const addItemButton = document.querySelector('button#addItemButton');
+// ahora las constantes para agregar elementos a la lista
+const addItemInput = document.querySelector("#addItem"); //input
+const addItemButton = document.querySelector("button#addItemButton");
 
-// eliminar ultimo item 
+// eliminar ultimo item
 
-const removeItemButton = document.querySelector('button#removeItemButton');
+const removeItemButton = document.querySelector("button#removeItemButton");
 
-// eliminnar elemento seleccionado 
+// eliminnar elemento seleccionado
 
+const removeItem = document.querySelector("button#removeItem");
 
-const removeItem = document.querySelector('button#removeItem');
-
-
-hide.addEventListener('click', () =>{
-    if(listDiv.style.display == 'none'){
-        listDiv.style.display = 'block';
-        hide.textContent = '<<';
-    }else{
-        listDiv.style.display = 'none';
-        hide.textContent = '>>';
+hide.addEventListener("click", () => {
+    if (listDiv.style.display == "none") {
+        listDiv.style.display = "block";
+        hide.textContent = "<<";
+    } else {
+        listDiv.style.display = "none";
+        hide.textContent = ">>";
     }
 });
 
-// agregar elementos a la lista 
+// agregar elementos a la lista
 
-addItemButton.addEventListener('click', () =>{
-    let list = document.querySelector('ul');
-    let li = document = document.createElement('li');
+addItemButton.addEventListener("click", () => {
+    let list = document.querySelector("ul");
+    let li = (document = document.createElement("li"));
     li.textContent = addItemInput.value;
-    list.appendChild(li); //esto para agregar elementos a la lista 
-    addItemInput.value="";
-
-    
+    list.appendChild(li); //esto para agregar elementos a la lista
+    addItemInput.value = "";
 });
 
-// eliminar ultimo elemento de la lista 
+// eliminar ultimo elemento de la lista
 
-removeItemButton.addEventListener('click', ()=>{
-    let list = document.querySelector('ul');
-    let li = document.querySelector('ul');
+removeItemButton.addEventListener("click", () => {
+    let list = document.querySelector("ul");
+    let li = document.querySelector("li:last-child");
     list.removeChild(li);
 });
 
-
-// obtener index del elemto seleccionado 
-function findIndex (elem){
-    var i, len = items.length;
-    for(i=0; i<len; i++) {
-        if(items[i]===elem){
+// obtener index del elemto seleccionado
+function findIndex(elem) {
+    var i,
+        len = items.length;
+    for (i = 0; i < len; i++) {
+        if (items[i] === elem) {
             return i;
         }
     }
     return -1;
-};
+}
 
 var list = document.getElementById("listItems"),
     items = list.getElementsByTagName("li");
-list.onclick = function(e){
-    var event = e || window.event, 
-    src = event.target || event.srcElement;
+list.onclick = function (e) {
+    var event = e || window.event,
+        src = event.target || event.srcElement;
     var myIndex = findIndex(src);
-    
+
     // alert (myIndex )
     index = myIndex;
     console.log(myIndex);
-    
+
     // cambiar color del elemento seleccionado
-    if (event.target.tagName === 'LI'){
-    list.querySelector('li').forEach(el =>el.classList.remove('alert', 'alert-success')); //se agregan clase de bootstarp para los elemntos 
-    items[index].classList.ass('alert', 'alert-success');
-}
+    if (event.target.tagName === "LI") {
+        list.querySelectorAll("li").forEach(el => el.classList.remove("alert", "alert-success")); //se agregan clase de bootstarp para los elementos
+        items[index].classList.add("alert", "alert-success");
+    }
 };
 
-// eliminar elemento seleccionado 
+// eliminar elemento seleccionado
 
-removeItem.addEventListener('click', () =>{
-    let showDiv = document.getElementById('list');
-    let divAlert = document.createElement('div');
-    let label = document.createElement('label');
-    label.innerHTML= 'Se elimino el elemento'+index;
-    divAlert.classList.add('alert', 'alert-danger'); //clase de alerta 
+removeItem.addEventListener("click", () => {
+    let showDiv = document.getElementById("list");
+    let divAlert = document.createElement("div");
+    let label = document.createElement("label");
+    label.innerHTML = "Se elimino el elemento " + index;
+    divAlert.classList.add("alert", "alert-danger"); //clase de alerta
 
     items[index].parentNode.removeChild(items[index]);
     divAlert.appendChild(label);
     showDiv.appendChild(divAlert);
-    
 
     let identificadorTiempo;
-    function temporizador (){
+    function temporizador() {
         identificadorTiempo = setTimeout(divAlert, 3000);
     }
-    
-
 });
